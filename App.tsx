87886f0e3f9ac1e9fc6b13bc75d724ed0c06341b
bglgwyng <bglgwyng@gmail.com>
2023-09-06 14:37:14 +0900

    Add filp.

--- App.tsx
+++ App.tsx
@@ -5,25 +5,46 @@
  * @format
  */
 
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
 import {
   Camera,
   useCameraDevices,
+  useFrameProcessor,
 } from 'react-native-vision-camera';
 
 function App(): JSX.Element {
-  const device = useCameraDevices().front;
+  const [direction, setDirection] = useState<'back' | 'front'>('back');
+  const device = useCameraDevices("wide-angle-camera")[direction];
+
+  useEffect(() => {
+    void (async() => {
+      await Camera.requestCameraPermission();
+    })()
+  }, [])
+
+  const frameProcessor = useFrameProcessor(() => {
+    'worklet';
+  }, [])
+
+  const cameraRef = useRef<Camera>(null)
 
   return (
-    <View style={{ flex: 1, backgroundColor: 'red' }}>
-      {device && (
-        <Camera
-          device={device}
-          isActive
-          style={{flex: 1}}
-        />
-      )}
+    <View style={{ flex: 1 }}>
+      <View style={StyleSheet.absoluteFill}>
+        {device && (
+          <Camera
+            ref={cameraRef}
+            device={device}
+            style={{ flex: 1 }}
+            fps={30}
+            frameProcessor={frameProcessor}
+            video={false}
+            isActive />
+        )}
+      </View>
+      <View style={{ flex: 1 }}></View>
+      <Button title="flip" onPress={() => { setDirection(direction === 'back' ? 'front' : 'back') }} />
     </View>
   );
 }
